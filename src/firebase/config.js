bf354e8e1b8596fa91ec209cb45b797ab0d398c0
delfinarawson <drawson@udesa.edu.ar>
2023-11-01 15:20:56 -0300

    archivos estructura

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC275ow-53-3Asmxu6QTiPF-2V1t1iNRiw",
+  authDomain: "prog3final-20679.firebaseapp.com",
+  projectId: "prog3final-20679",
+  storageBucket: "prog3final-20679.appspot.com",
+  messagingSenderId: "59075933964",
+  appId: "1:59075933964:web:00c80d8a4d0791be42fb29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
