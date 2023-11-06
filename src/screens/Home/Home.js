b0d980cdc372628b3130eb3eef64bd6448c3e557
delfinarawson <drawson@udesa.edu.ar>
2023-11-06 16:44:50 -0300

    cambios

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,21 +1,43 @@
 import react, { Component } from 'react';
-import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
-import { auth } from '../../firebase/config';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet, FlatList} from 'react-native';
+import { db, auth } from '../../firebase/config';
+import PostForm from '../PostForm/PostForm';
+import Post from '../../components/Post/Post'; 
 
 class Home extends Component {
     constructor(){
         super()
         this.state={
-          
+            posts: []
         }
+    }
+
+    componentDidMount(){
+        //Traer los datos de Firebase y cargarlos en el estado.
+        db.collection('posts').onSnapshot(
+            listaPosts => {
+                let postsAMostrar = [];
+
+                listaPosts.forEach(unPost => {
+                    postsAMostrar.push({
+                        id:unPost.id,
+                        datos: unPost.data()            
+                    })
+                })
+
+                this.setState({
+                    posts:postsAMostrar
+                })
+            }
+        )
+
     }
 
     logout(){
         auth.signOut();
-
         this.props.navigation.navigate('Login')
-
     }
+
 
 
     render(){
@@ -28,7 +50,21 @@ class Home extends Component {
                 <TouchableOpacity onPress={ () => this.props.navigation.navigate('Mi perfil')}>
                    <Text>Mi perfil</Text>
                 </TouchableOpacity>
+
+                <Text>Crear nuevo post</Text>
+                <PostForm />
+
+                <Text>Lista de posteos creados</Text>
+                
+                <FlatList
+                    data={this.state.posts}
+                    keyExtractor={ unPost => unPost.id }
+                    renderItem={ ({item}) => <Post dataPost = {item} />  }
+                />
+
             </View>
+
+
         )
     }
 }
